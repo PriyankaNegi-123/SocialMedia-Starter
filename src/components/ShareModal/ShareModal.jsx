19b157b444fa4a3544516fa2278f348955d9274c
Priyanka Negi <negipriyanka2211@gmail.com>
2023-05-23 21:32:38 +0530

    third

---
+++ src/components/ShareModal/ShareModal.jsx
@@ -0,0 +1,24 @@
+
+import { Modal, useMantineTheme } from '@mantine/core';
+import PostShare from '../postShare/PostShare';
+
+function ShareModal({modalOpened, setModalOpened}) {
+
+  const theme = useMantineTheme();
+
+  return (
+    
+      <Modal
+        overlaycolor= {theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
+        overlayopacity= {0.55}
+        overlayblur= {3}
+        size='55%'
+        opened={modalOpened}
+        onClose={()=>setModalOpened(false)}
+      >
+      <PostShare/>
+      </Modal>
+  );
+}
+
+export default ShareModal
